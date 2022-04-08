fcd429e7029dc44fe650a83d19e4824ea9cd1300
LuisBalan <jlbalcz@gmail.com>
2022-04-08 11:14:33 -0500

    add chapter03: defining tables

---
+++ Learn_SQL_book_practices/chapter03_defining_tables.sql
@@ -0,0 +1,10 @@
+-- syntax for creating tables
+
+/* CREATE TABLE table_name (
+    columnn1_name1 datatype [column constraints],
+    columnn2_name2 datatype [column constraints],
+    ...
+    [table_constraints],
+    [table_constraints]
+) */
+
