1fdd16c1a5be9905e6664b4db8df663fd8baa2fc
LuisBalan <jlbalcz@gmail.com>
2022-04-22 01:01:20 -0500

    add count function examples

--- Learn_SQL_book_practices/chapter06_selecting_data_part2.sql
+++ Learn_SQL_book_practices/chapter06_selecting_data_part2.sql
@@ -16,3 +16,17 @@ SELECT SUBSTRING(<string>, index)  : in SQL indexes start at 1;
 SELECT NOW() current today info;
 SELECT CURDATE(); 
 */
+
+/*
+Other functions: 
+
+COUNT() : It counts all the non-empty values:
+
+SELECT COUNT(*) FROM employees;
+SELECT COUNT(contact_number) FROM employees;
+SELECT COUNT(DISTINCT(years_in_company)) FROM employees;
+SELECT COUNT(years_in_company) FROM employees;
+SELECT COUNT(DISTINCT(salary)) FROM employees;
+SELECT COUNT(salary) FROM employees;
+
+*/
