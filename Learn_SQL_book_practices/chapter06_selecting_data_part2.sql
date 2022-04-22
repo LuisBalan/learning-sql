c5967989294cfa2677a3ff00a0ee5c1bcb8d0168
LuisBalan <jlbalcz@gmail.com>
2022-04-22 00:46:35 -0500

    add chapter06 file

---
+++ Learn_SQL_book_practices/chapter06_selecting_data_part2.sql
@@ -0,0 +1,18 @@
+/*
+Some functions in SQL:
+CONCAT()
+SUBSTRING()
+NOW()
+CURDATE()
+*/
+
+/*
+Ejemplos de uso:
+*/
+
+/*
+SELECT CONCAT("Hello", "World!");
+SELECT SUBSTRING(<string>, index)  : in SQL indexes start at 1;
+SELECT NOW() current today info;
+SELECT CURDATE(); 
+*/
