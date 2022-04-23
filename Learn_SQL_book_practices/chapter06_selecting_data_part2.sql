911f38b6169df501d117bbec88ddec3d7e897a09
LuisBalan <jlbalcz@gmail.com>
2022-04-23 16:33:08 -0500

    add examples using functions

--- Learn_SQL_book_practices/chapter06_selecting_data_part2.sql
+++ Learn_SQL_book_practices/chapter06_selecting_data_part2.sql
@@ -14,7 +14,8 @@ Ejemplos de uso:
 SELECT CONCAT("Hello", "World!");
 SELECT SUBSTRING(<string>, index)  : in SQL indexes start at 1;
 SELECT NOW() current today info;
-SELECT CURDATE(); 
+SELECT CURDATE(); It gives the current date
+SELECT CURTIME(); It gives the current time
 */
 
 /*
@@ -28,5 +29,29 @@ SELECT COUNT(DISTINCT(years_in_company)) FROM employees;
 SELECT COUNT(years_in_company) FROM employees;
 SELECT COUNT(DISTINCT(salary)) FROM employees;
 SELECT COUNT(salary) FROM employees;
+
+*/
+
+/* Average, Round, Max, Min, Sum
+AVG()
+
+Examples:
+SELECT AVG(salary) FROM employees;
+
+ROUND()
+SELECT ROUND(AVG(salary)) FROM employees;
+
+MAX()
+SELECT MAX(salary) FROM employees;
+
+MIN()
+SELECT MIN(salary) FROM employees;
+
+SUM()
+SELECT SUM(salary) FROM employees;
+*/
+
+/* GROUP BY
+
 
 */
