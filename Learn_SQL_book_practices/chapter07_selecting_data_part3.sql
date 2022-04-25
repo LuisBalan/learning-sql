e99f7e665b6e896cdfbba71b1d539b2ceb6193c5
LuisBalan <jlbalcz@gmail.com>
2022-04-24 20:09:28 -0500

    add chapter07 file

---
+++ Learn_SQL_book_practices/chapter07_selecting_data_part3.sql
@@ -0,0 +1,13 @@
+/*
+Sintaxys for joins:
+*/
+
+/*
+SELECT [table_names.] columns_names_or_other_information
+FROM
+left_table
+JOIN / INNER JOIN / LEFT JOIN / RIGHT JOIN
+right_table
+on
+left_table.column_name = right_table.column_name;
+*/
