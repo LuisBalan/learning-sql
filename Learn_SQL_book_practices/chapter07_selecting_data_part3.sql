0e5610b82ee4433001693076c70726cee8b13da2
LuisBalan <jlbalcz@gmail.com>
2022-04-24 21:23:33 -0500

    add examples of UNION and UNION ALL clauses

--- Learn_SQL_book_practices/chapter07_selecting_data_part3.sql
+++ Learn_SQL_book_practices/chapter07_selecting_data_part3.sql
@@ -39,3 +39,17 @@ SELECT employees.id, employees.em_name, employees.gender, employees.salary, ment
 mentorships.mentee_id AS 'Mentee', mentorships.project FROM employees RIGHT JOIN mentorships
 ON employees.id = mentorships.mentor_id;
 */
+
+/* UNION
+With UNION we join the results of two SELECT. It's necessary that both SELECT have the same number of columns.
+The result is a table with first amount of rows correspond to first SELECT and the last corresponds to second one.
+
+Example:
+
+SELECT id, em_name FROM employees WHERE Salary > 5000 UNION SELECT mentor_id, project FROM mentorships; 
+SELECT  em_name, salary FROM employees WHERE gender = "M" UNION SELECT em_name, years_in_company FROM employees WHERE gender = "F";
+SELECT em_name FROM employees WHERE Salary > 10000 UNION SELECT mentor_id FROM mentorships;
+SELECT em_name FROM employees WHERE Salary > 10000 UNION ALL SELECT mentor_id FROM mentorships;
+SELECT Salary, em_name, years_in_company FROM employees WHERE gender = "F" UNION SELECT mentor_id, project, status FROM mentorships;
+SELECT Salary, em_name, years_in_company FROM employees WHERE gender = "F" UNION ALL SELECT mentor_id, project, status FROM mentorships;
+*/
