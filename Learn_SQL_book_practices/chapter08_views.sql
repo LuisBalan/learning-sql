d41db7f2d275458a855e721d698ac7d8e51cb561
LuisBalan <jlbalcz@gmail.com>
2022-04-30 00:51:29 -0500

    add ALTER and DELETE view

--- Learn_SQL_book_practices/chapter08_views.sql
+++ Learn_SQL_book_practices/chapter08_views.sql
@@ -25,3 +25,32 @@ An example of query on a view, we can write:
 SELECT mentor_id, `Project Name` FROM myView;
 
 */
+
+/* Altering a View
+After creating a view, if we want to make any change to it, we use the
+ALTER VIEW keywords.
+
+The syntax is
+
+ALTER VIEW name_of_view AS SELECT statement;
+
+For instance:
+
+ALTER VIEW myView AS SELECT employees.id, mentorships.mentor_id, employees.em_name
+AS 'Mentor', mentorships.project AS 'Project' FROM mentorships
+JOIN employees ON employees.id = mentorships.mentor_id;
+
+*/
+
+/* DELETING VIEWS
+If we want to delete a view we use the DROP VIEW keywords. The syntax is:
+
+DROP VIEW IF EXISTS name_of_view;
+
+To delete myView we write;
+
+DROP VIEW IF EXISTS myView;
+
+
+
+*/
