46c71ed6315b99fc2e6bf8b3aa4377a13443ba7e
LuisBalan <jlbalcz@gmail.com>
2022-05-05 13:58:42 -0500

    add examples of assgination of functions to variables

--- Learn_SQL_book_practices/chapter10_variables_stored_routines.sql
+++ Learn_SQL_book_practices/chapter10_variables_stored_routines.sql
@@ -13,3 +13,30 @@ SELECT * FROM mentorships WHERE mentor_id = @em_id;
 SELECT * FROM mentorships WHERE mentee_id = @em_id;
 SELECT * FROM employees WHERE id = @em_id;
 */
+
+/*
+SET @em_1 = 2;
+*/
+
+/* We can also assign strigs to SQL variables, for example:
+
+SET @em_name = 'Jamie';
+*/
+
+/*
+We can also add the result of an operation to a variable. For example:
+
+SET @price = 12;
+SET @price = @price + 3;
+
+*/
+
+
+/*
+Also, we can add the result of a function to a variable. For example:
+SET @result = SQRT(9);
+
+In order to visualize the value of the variable we can type:
+SELECT @result;
+
+*/
